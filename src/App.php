15ded544aa87866f51fce38369a3fbc15d03166e
choesu <sunhak.choe@gmail.com>
2025-09-08 02:37:37 +0000

    create database Books

---
+++ src/App.php
@@ -0,0 +1,19 @@
+<?php
+namespace Jm\Webproject;
+// declare other components used in this class
+use Dotenv;
+use \Exception;
+
+class App {
+    // visibility
+    public function __construct() {
+        // get the root directory or Current Working Directory
+        $path = getcwd();
+        // pass the path to Dotenv to load the .env file
+        $dotenv = Dotenv\Dotenv::createImmutable( $path );
+        // load the .env configuration into $_ENV (super global)
+        $dotenv -> load();
+    }
+    
+}
+?>
